848186d782321f702bb32729133c5b04b52d7fda
Armita2oo3 <mir.armita17@gmail.com>
2024-08-09 13:57:07 +0330

    added the office and avatar

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,53 @@
-import { OrbitControls } from "@react-three/drei";
+import {
+    ContactShadows,
+    Environment,
+    OrbitControls,
+    Sky,
+} from "@react-three/drei";
+import { Avatar } from "./Avatar";
+import { useControls } from "leva";
+import { Office } from "./Office";
+import { MyOffice } from "./MyOffice";
 
 export const Experience = () => {
-  return (
-    <>
-      <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
-    </>
-  );
+    const { animation } = useControls({
+        animation: {
+            value: "Typing",
+            Options: ["Typing", "Falling", "Standing"],
+        },
+    });
+    return (
+        <>
+            <OrbitControls />
+            <Sky />
+            <Environment preset='sunset' />
+            <group position-y={-1}>
+                <ContactShadows
+                    opacity={0.42}
+                    scale={10}
+                    blur={1}
+                    far={10}
+                    resolution={256}
+                    color='#000000'
+                />
+                <Avatar animation={animation} />
+                {animation === "Typing" && (
+                    <mesh scale={[0.8, 0.5, 0.8]} position-y={0.25}>
+                        <boxGeometry />
+                        <meshStandardMaterial color='white' />
+                    </mesh>
+                )}
+                <mesh
+                    receiveShadow
+                    scale={5}
+                    rotation-x={-Math.PI * 0.5}
+                    position-y={-0.001}
+                >
+                    <planeGeometry />
+                    <meshStandardMaterial color='white' />
+                </mesh>
+                <MyOffice />
+            </group>
+        </>
+    );
 };
